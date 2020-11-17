32b3e57bdf1e8355f796c721e0dca20c2cf289c3
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-17 21:15:29 +0500

    add library

--- animation-in-react/src/App.js
+++ animation-in-react/src/App.js
@@ -1,19 +1,36 @@
 import React, { useRef,useEffect } from 'react';
+import useWebAnimations from "@wellyshen/use-web-animations";
 import './App.css';
 
 function App() {
-  const element = useRef(null);
+  const { ref, playState } = useWebAnimations({
+    keyframes: {
+      transform: ["translateX(500px)"], // Move by 500px
+      background: ["red", "blue", "green"], // Go through three colors
+    },
+    timing: {
+      delay: 500, // Start with a 500ms delay
+      duration: 1000, // Run for 1000ms
+      iterations: Infinity, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    },
+  //   onReady: ({ playState, animate, animation }) => {
+  //     // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+  //   },
+  //   onUpdate: ({ playState, animate, animation }) => {
+  //     // Triggered when the animation enters the running state or changes state
+  //   },
+  //   onFinish: ({ playState, animate, animation }) => {
+  //     // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+  //   },
+  //   // More useful options...
+  });
 
-  useEffect(() => {
-    element.current.focus();
-  }, [])
+
 
   return (
-    <div >
-      <input ref={element} type="text" />
-      <input  type="text" />
-
-    </div>
+    <div className="div">Hello World..!   </div>
   );
 }
 
