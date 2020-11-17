76f28f658bafab9a553946627bd9f18c51ff51e4
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-17 11:28:23 +0500

    created react app and another folder of simple animations

---
+++ simple-animation/index.js
@@ -0,0 +1,12 @@
+
+
+var elem = document.getElementById("bird")
+var animation = elem.animate([
+    { transform: "translate(0,0)" },
+    { transform: "translate(100%,0)" }
+], {
+    duration: 5000,
+    iterations: Infinity,
+    direction: "normal"
+})
+
