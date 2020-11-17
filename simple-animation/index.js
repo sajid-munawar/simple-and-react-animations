b4a521eab0b75bb95a31aaffcb15f7ad32635e63
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-17 11:36:54 +0500

    change direction of elements

--- simple-animation/index.js
+++ simple-animation/index.js
@@ -1,5 +1,15 @@
 
 
+var elem = document.getElementById("cloud")
+var animation = elem.animate([
+    { transform: "translate(0,0)" },
+    { transform: "translate(100%,0)" }
+], {
+    duration: 5000,
+    iterations: Infinity,
+    direction: "reverse"
+})
+
 var elem = document.getElementById("bird")
 var animation = elem.animate([
     { transform: "translate(0,0)" },
